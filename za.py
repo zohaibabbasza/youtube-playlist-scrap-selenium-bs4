197f93b2ebece1e8e653db63186f31eef3e8970a
zohaibabbasza <zohaibabbasza@outlook.com>
2019-07-02 03:12:31 +0500

    za

--- za.py
+++ za.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup as bs
 import requests
+import csv
+from itertools import zip_longest
+
 
 def get_link_id(artist):
     #LINK ID HERE
@@ -10,20 +13,29 @@ def get_link_id(artist):
     page = r.text
     soup=bs(page,'html.parser')
     za=soup.find_all('a',{'class':'yt-lockup-playlist-item-title yt-uix-sessionlink spf-link'})
-    print(len(za))
     i =0
     ids = []  
     while i != len(za):
         z = za[i].get('href').split('list=')
-        print(z[1])
         ids.append(z[1])
         i+=2
     return ids
+
+def write_to_csv(artist,links,titles):
+    d = [titles, links]
+    export_data = zip_longest(*d, fillvalue = '')
+    with open(artist+'.csv', 'a', encoding="utf-8", newline='') as myfile:
+        wr = csv.writer(myfile)
+        wr.writerow(("titles", "links"))
+        wr.writerows(export_data)
+    myfile.close()
+ 
 
 
 def get_url_title(artist):
     list_ids = []
     list_ids = get_link_id(artist)
+    print("scraping started....")
     for k in list_ids:
         for_title = 'https://www.youtube.com/watch?v=pRpeEdMmmQ0&list='
         for_url = 'https://www.youtube.com/playlist?list='
@@ -38,19 +50,24 @@ def get_url_title(artist):
         soup=bs(page,'html.parser')
         h4 = soup.find_all("h4",text=True)
         h4 = h4[:-6]
-        print(len(links))
-        print(len(h4))
-        for l in links:
-            print('www.youtube.com' + l.get('href'))
-        
-        for i in h4:
-            print(i.text)
-
+        titles = []
+        link = []
+        for l in  range(len(links)):
+            try:
+                titles.append(h4[l].text)
+            except IndexError:
+                titles.append(h4[l].text)
+            try:
+                link.append('www.youtube.com' + links[l].get('href'))
+            except IndexError:
+                link.append('null')    
+        write_to_csv(artist,link,titles)
+    print("Successfully Done")
 
 def main():
     print("Input Artist Name:")
     artist = input()
     get_url_title(artist)
-
+    #single(artist)
 
 main()
